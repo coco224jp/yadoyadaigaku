768e8e5f3cfbd827dbf1388620b25ca7b510d322
nicotto-hattori <hattori@nicottolab.com>
2025-10-22 13:07:44 +0900

    ホテルマネジメントの教科書、テンプレート追加（デフォルトの投稿からカスタム投稿へ変更）、CSS修正

---
+++ archive-knowledge.php
@@ -0,0 +1,71 @@
+<?php
+  $theme_uri = get_template_directory_uri();
+
+  get_header();
+  $args = [
+    'ttl_jp' => 'ホテルマネジメントの<br class="only-sp">教科書一覧',
+    'ttl_en' => 'Knowledge'
+  ];
+  get_template_part('template-parts/subpage-mv', null, $args);
+?>
+
+<main>
+<section class="p-blog-archive">
+  <div class="l-inner">
+    <h2 class="c-subpage-ttl __small text-center">ホテルマネジメントの教科書とは</h2>
+    <p class="p-blog-archive__intro-txt">各業界のスペシャリストをお招きして開催しているオープン講座です。2,000年に始まり、過去の500回以上開催している宿泊ビジネス・観光業に関する講演会。基本月一回、平日の18時～19時30分に開講しています。</p>
+    <?php if(have_posts()): ?>
+    <ul class="p-blog-archive__list">
+      <?php while(have_posts()): the_post(); 
+            $cats = get_the_terms(get_the_ID(), 'knowledge-cat');
+
+            if($cats):
+              foreach($cats as $cat):
+                if($cat->slug !== 'uncategorized'):
+                  $cat_name = $cat->name;
+                  break;
+                endif;
+              endforeach;
+            endif;
+      ?>
+      <li class="p-blog-archive__item c-thumbnail-card">
+        <a href="<?php the_permalink(); ?>" class="c-thumbnail-card__inner">
+          <div class="c-thumbnail-card__body">
+            <?php if($cat_name): ?>
+            <span class="c-thumbnail-card__cat"><?php echo esc_html( $cat_name ); ?></span>
+            <?php endif; ?>
+            <h3 class="c-thumbnail-card__ttl"><?php the_title(); ?></h3>
+            <div class="c-thumbnail-card__date"><time datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time></div>
+            <p class="p-blog-archive__item-txt">
+            <?php
+              $content = strip_tags( get_the_content() ); // HTMLタグ除去
+              $trimmed = mb_substr( $content, 0, 140 ); // 140文字に制限
+              if ( mb_strlen( $content ) > 140 ) {
+                $trimmed .= '...';
+              }
+              echo esc_html( $trimmed );
+            ?>
+            </p>
+          </div>
+          <figure class="c-thumbnail-card__img">
+          <?php if(has_post_thumbnail()): ?>
+            <?php the_post_thumbnail('full'); ?>
+          <?php endif; ?>
+          </figure>
+        </a>
+      </li>
+      <?php endwhile; ?>
+    </ul>
+    <?php 
+      endif;
+
+      the_posts_pagination( array(
+        'mid_size' => 4,
+        'prev_text' => '',
+        'next_text' => '',
+      ) ); 
+    ?>
+  </div>
+</section>
+</main>
+<?php get_footer(); ?>
---
+++ single-knowledge.php
@@ -0,0 +1,145 @@
+<?php
+  $theme_uri = get_template_directory_uri();
+  $post_id = get_the_ID();
+
+  get_header();
+
+  $args = ['ttl_jp' => 'ホテルマネジメントの<br class="only-sp">教科書', 'ttl_en' => 'Knowledge'];
+  get_template_part('template-parts/subpage-mv', null, $args); 
+?>
+<main>
+  <section class="p-blog-single">
+    <div class="l-inner __small">
+      <?php while(have_posts()): the_post(); ?>
+        <h1 class="p-blog-single__ttl"><?php the_title(); ?></h1>
+        <div class="p-blog-single__date">
+          <time datetime="<?php echo get_the_date('Y-m-d'); ?>"><?php the_time('Y.m.d(D)'); ?></time>
+        </div>
+        <div class="p-blog-single__tags">
+          <?php
+            $tags = get_the_terms($post->ID, 'knowledge-tag');
+
+            if($tags):
+              foreach($tags as $tag):
+                echo '<span class="u-term">' . esc_html( $tag->name ) . '</span>';
+              endforeach;
+            endif;
+          ?>
+        </div>
+        <?php if(has_post_thumbnail()) : ?>
+        <figure class="p-blog-single__img">
+          <?php the_post_thumbnail('full'); ?>
+        </figure>
+        <?php endif; ?>
+        <div class="l-wp-block-content">
+          <?php the_content(); ?>
+        </div>
+        <div class="p-blog-single__tags">
+          <?php
+            if($tags):
+              foreach($tags as $tag):
+                echo '<span class="u-term">' . $tag->name . '</span>';
+              endforeach;
+            endif;
+          ?>
+        </div>
+        <?php
+          $prev_post = get_previous_post();
+          $next_post = get_next_post();
+          $is_navi_exist = (isset($prev_post) && $prev_post != '') || (isset($next_post) && $next_post != '');
+
+          if($is_navi_exist):
+        ?>
+        <nav class="p-blog-single__navi-wrap">
+        <?php
+            if (isset($prev_post) && $prev_post != ''):
+        ?>
+          <div class="p-blog-single__navi prev">
+            <a class="p-blog-single__navi-link" href="<?php echo get_permalink($prev_post->ID); ?>">
+              <span class="p-blog-single__navi-txt">前の記事</span>
+              <span class="p-blog-single__navi-ttl"><?php echo esc_html( get_the_title($prev_post->ID) ); ?></span>
+            </a>
+          </div>
+          <?php 
+            endif;
+            if (isset($next_post) && $next_post != ''):
+          ?>
+          <div class="p-blog-single__navi next">
+            <a class="p-blog-single__navi-link " href="<?php echo get_permalink($next_post->ID); ?>">
+              <span class="p-blog-single__navi-txt">次の記事</span>
+              <span class="p-blog-single__navi-ttl"><?php echo esc_html( get_the_title($next_post->ID) ); ?></span>
+            </a>
+          </div>
+          <?php 
+            endif;
+          ?>
+        </nav>
+        <?php 
+            endif;
+          endwhile;
+        ?>
+    </div><!-- /.l-inner -->
+  </section>
+
+  <section class="p-blog-single-other">
+    <div class="l-inner __small">
+      <h2 class="c-subpage-ttl">その他の記事</h2>
+      <div id="js-product-swiper" class="u-product-swiper swiper">
+        <ul class="swiper-wrapper">
+          <?php
+            $posts_args = [
+              'post_type' => 'knowledge',
+              'posts_per_page' => 5,
+              'post_status' => 'publish',
+              'orderby' => 'date',
+              'order' => 'DESC',
+            ];
+
+            $posts_query = new WP_Query($posts_args);
+
+            while($posts_query->have_posts()):
+              $posts_query->the_post();
+
+              $post_cats = get_the_terms(get_the_ID(), 'knowledge-cat');
+              $post_cat_name = '';
+
+              if($post_cats):
+                foreach($post_cats as $post_cat):
+                  $post_cat_name = $post_cat->name;
+                  break;
+                endforeach;
+              endif;
+          ?>
+          <li class="c-thumbnail-card swiper-slide">
+            <a href="<?php the_permalink(); ?>" class="c-thumbnail-card__inner">
+              <div class="c-thumbnail-card__body">
+                <span class="c-thumbnail-card__cat"><?php echo $post_cat_name; ?></span>
+                <h3 class="c-thumbnail-card__ttl"><?php the_title(); ?></h3>
+                <div class="c-thumbnail-card__date">
+                  <time datetime="<?php echo esc_attr(get_the_date('Y-m-d')); ?>"><?php the_time('Y.m.d(D)'); ?></time>
+                </div>
+              </div>
+              <?php if(has_post_thumbnail()): ?>
+              <figure class="c-thumbnail-card__img">
+                <?php the_post_thumbnail('full'); ?>
+              </figure>
+              <?php endif; ?>
+            </a>
+          </li>
+          <?php
+            endwhile;
+            wp_reset_postdata();
+          ?>
+        </ul>
+      </div>
+      <div id="js-product-swiper-controls" class="c-swiper-controls">
+        <div class="swiper-button-prev c-arrow-btn prev"></div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-next c-arrow-btn next"></div>
+      </div> 
+    </div>
+  </section>
+</main>
+<?php
+  get_footer();
+?>
